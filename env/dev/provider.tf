1fc22b79ab2a323a5e25bc81772eeeb7d3b6780b
devopslabnotes <anuj.k@devopslabnotes.com>
2025-07-14 13:30:52 -0400

    Update provider.tf

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -1,6 +1,4 @@
 provider "google" {
     project = var.project_id
     region = var.region
-    impersonate_service_account = var.impersonate_service_account
-    Workload_identity_provider = var.Workload_identity_provider
 }
