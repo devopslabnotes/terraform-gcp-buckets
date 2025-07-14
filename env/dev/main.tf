7b41308f3a1af6d930033f500f941d615a1e6c16
devopslabnotes <anuj.k@devopslabnotes.com>
2025-07-14 12:56:37 -0400

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -3,6 +3,6 @@ module "gcs_bucket" {
     bucket_name = var.bucket_name
     location = var.region
     force_destroy = false
-    versioining_enabled = true
+    versioning_enabled = true
     lifecycle_age = 30
 }
