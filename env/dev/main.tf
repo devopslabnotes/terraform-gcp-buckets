31ad26c9515d2f7954952fafb14105c368756623
devopslabnotes <anuj.k@devopslabnotes.com>
2025-07-14 12:45:13 -0400

    Update main.tf
    variable bucket name

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,6 +1,6 @@
 module "gcs_bucket" {
     source = "../../modules/gcs_bucket"
-    bucket_name = "devopslabnotes-bucket-dev"
+    bucket_name = var.bucket_name
     location = var.region
     force_destroy = false
     versioining_enabled = true
