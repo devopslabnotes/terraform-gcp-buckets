38054dbac0137405977b68348e743299f6984e26
devopslabnotes <anuj.k@devopslabnotes.com>
2025-07-14 13:35:37 -0400

    Update variables.tf

--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -12,13 +12,3 @@ variable "region" {
     description = "Region where resources will be deployed"
     type = string
 }
-
-variable "Workload_identity_provider" {
-    description = "Workload Identity provider resource ID"
-    type = string
-}
-
-variable "impersonate_service_account" {
-    description = "Email of the service account to impersonate"
-    type = string
-}
