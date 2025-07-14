cce67f24d2f3047d922e798fd643871d418620ce
devopslabnotes <anuj.k@devopslabnotes.com>
2025-07-14 12:53:57 -0400

    Update provider.tf

--- env/dev/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-    GOOGLE_WORKLOAD_IDENTITY_PROVIDER = var.GOOGLE_WORKLOAD_IDENTITY_PROVIDER
-    GOOGLE_SERVICE_ACCOUNT_EMAIL = var.GOOGLE_SERVICE_ACCOUNT_EMAIL
-    project = var.project_id
-    region = var.region
-}
