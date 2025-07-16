a55bc5afe4ba30ffa502c1b0ea12a8e95bda2a55
devopslabnotes <anuj.k@devopslabnotes.com>
2025-07-16 17:06:57 -0400

    Update main.tf

--- modules/gcs_bucket/main.tf
+++ modules/gcs_bucket/main.tf
@@ -14,6 +14,8 @@ resource "google_storage_bucket" "this" {
     location = var.location
     force_destroy = var.force_destroy
 
+    uniform_bucket_level_access = true
+
     versioning {
         enabled = var.versioning_enabled
     }
